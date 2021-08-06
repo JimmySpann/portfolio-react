24b34da149329050fcf5c4f6428e6258b4378c17
jimmyspann <jimmyleespann@outlook.com>
2021-08-06 18:52:11 -0400

    add utils

---
+++ src/utils/customDebugging.js
@@ -0,0 +1,14 @@
+class Debugger {
+  static isDebugOn = true;
+}
+
+export default function debugLog(title, value) {
+  if (Debugger.isDebugOn) {
+    if (!value) {
+      const variableName = Object.keys({ title })[0];
+      console.log(variableName, title);
+    } else {
+      console.log(title, value);
+    }
+  }
+}
